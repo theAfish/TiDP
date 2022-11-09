111954bdce3f5acb34ccfe77bdf5fd8974657e41
theAfish <85424334+theAfish@users.noreply.github.com>
2022-11-09 17:16:19 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from TaichiDeepPot.tidp import TiDP as td
-import taichi as ti
-
-arch = ti.vulkan if ti._lib.core.with_vulkan() else ti.cuda
-ti.init(arch=arch)
 
 mdl = td(graphs=["graphs/graph.000.pb", "graphs/graph.001.pb", "graphs/graph.002.pb", "graphs/graph.003.pb"],
          structure="poscars/pbi2.poscar",
