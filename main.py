43319d0191b6675cfcdbab1b6d4a99414df23b1d
theAfish <85424334+theAfish@users.noreply.github.com>
2022-11-09 16:44:58 +1100

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ mdl = td(graphs=["graphs/graph.000.pb", "graphs/graph.001.pb", "graphs/graph.002
          type_map={"I": 0, "Pb": 2},
          res=(512,512))
 
+# show model deviation
 mdl.set_prob_atom(5)
 mdl.show()
+
+# # run a simple DPMD
+# mdl.run()
