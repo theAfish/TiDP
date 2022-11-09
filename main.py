fc5d0022cd74b10d890ae26b0155e8b83cbfdd2f
theAfish <85424334+theAfish@users.noreply.github.com>
2022-11-09 17:52:08 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ mdl = td(graphs=["graphs/graph.000.pb", "graphs/graph.001.pb", "graphs/graph.002
          type_map={"I": 0, "Pb": 2},
          res=(512,512))
 
-# show model deviation
+# set the probe atom according to its index
 mdl.set_prob_atom(5)
+### show model deviation ###
 mdl.show()
 
-# # run a simple DPMD
+# # comments above and uncomments below to run a simple DPMD
 # mdl.run()
